53654256f6324560800745079a2a471330d6adaa
Mehar-Zubair <140060027+Mehar-Zubair@users.noreply.github.com>
2023-07-24 21:28:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1sm
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
 global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
+global.location = 'Islamabad PK'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
+global.sudo = process.env.SUDO || '923004373535'
 global.devs = '919628516236';
 global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/57060e58f1ea8b8b9ebc8.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/t6VBCJL.jpg'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? '𝐎𝐩𝐞𝐧 𝐀𝐈 𝛁𝟑' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? '𝐌𝐞𝐡𝐚𝐫 𝙓𝙪𝙗𝙖𝙞𝙧' : process.env.OWNER_NAME,
@@ -21,7 +21,7 @@ module.exports = {
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? '𝐌𝐞𝐡𝐚𝐫 𝙓𝙪𝙗𝙖𝙞𝙧' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '𝐎𝐩𝐞𝐧 𝐀𝐈 𝛁𝟑' : process.env.PACK_INFO.split(";")[1],
-  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '971' : process.env.FAKE_COUNTRY_CODE,
@@ -45,7 +45,7 @@ module.exports = {
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR',
-  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
 
